702d1aa9ba9d97d58606ba66b164026d7bae0974
yuki3738 <y.minamiya@gmail.com>
2023-01-15 15:14:36 +0900

    Add 4.3

---
+++ 4/4.3/1.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    print(i)
+
+print(list(range(5, 10)))
+print(list(range(0, 10, 3)))
+print(list(range(-10, -100, -30)))
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
+
+print(range(10))
+
+print(sum(range(4)))
